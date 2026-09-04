5fd040aeb8765a3b5c313cae4d5340229697efe0
senumi nimsari <senuminimsari12@gmail.com>
2026-09-04 10:33:33 +0530

    shipments-bids

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ import firebase_setup  # noqa: F401  (import triggers Firebase initialization)
 from auth_routes import router as auth_router
 from agency_routes import router as agency_router
 from hscode_routes import router as hscode_router
+from shipment_routes import router as shipment_router
+from tender_routes import router as tender_router 
 
 app = FastAPI()
 
 app.include_router(auth_router)
 app.include_router(agency_router)
 app.include_router(hscode_router)
+app.include_router(shipment_router)
+app.include_router(tender_router)
 
 
 @app.get("/")
